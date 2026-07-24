1ba35bc66029eae4c04beb0f4cdcbe242b2a4236
khanmaaz77 <maazsameer4777@gmail.com>
2026-07-24 22:56:24 +0530

    Create app.vue

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app-root">
+    <Navbar />
+    <main class="container">
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: { Navbar }
+}
+</script>
